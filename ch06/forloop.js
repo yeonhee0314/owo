a0c6a91d805b8fac3061dcc6e725c821b06962b7
yeonhee0314 <aa77932492@gmail.com>
2026-03-26 16:15:41 +0900

    ch06

---
+++ ch06/forloop.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 5; i++) {
+    console.log("Hello World");
+}
+
+let dan = 7;
+for (let i = 1; i <= 9; i++) {
+    console.log(`${dan} x ${i} = ${dan * i}`);
+}
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+console.log(sum);
